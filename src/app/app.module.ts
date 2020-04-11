6ac10f61959f985b5f4e5aea525fa55204a1dab4
Raphael de Moraes <raphael.moraes@operacao.rcadigital.com.br>
2020-04-11 18:35:08 -0300

    Data Binding Examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -10,6 +11,7 @@ import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
@@ -19,9 +21,11 @@ import { rootRouterConfig } from './app.routes';
     FooterComponent,
     SobreComponent,
     ContatoComponent,
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule, 
+    FormsModule, 
     [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
   providers: [
