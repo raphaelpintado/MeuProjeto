192af2b2667976f220c96881fe03f0eefc4a3436
Raphael de Moraes <raphael.moraes@operacao.rcadigital.com.br>
2020-04-11 17:19:13 -0300

    Roteamento e Configuração

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,11 +17,16 @@ import { FooterComponent } from './navegacao/footer/footer.component';
     MenuComponent,
     HomeComponent,
     FooterComponent,
+    SobreComponent,
+    ContatoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
